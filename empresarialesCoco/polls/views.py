7ef26c48dd34873ac0661e174dc4cf831efef477
PilarArroyave <mdelpilar.arroyave@udea.edu.co>
2018-03-17 12:21:11 -0500

    Dojo listo

--- empresarialesCoco/polls/views.py
+++ empresarialesCoco/polls/views.py
@@ -9,6 +9,8 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
 
+from .models import Question, Choice
+
 class IndexView(generic.ListView):
     template_name='polls/index.html'
     context_object_name= 'latest_question_list'
@@ -19,7 +21,7 @@ class IndexView(generic.ListView):
 
 class DetailView(generic.DetailView):
     model=Question
-    template_name='polls/details.html'
+    template_name='polls/detail.html'
 
 class ResultsView(generic.DetailView):
     model=Question
@@ -28,14 +30,14 @@ class ResultsView(generic.DetailView):
 def vote(request, question_id):
     question=get_object_or_404(Question,pk=question_id)
     try: 
-        selected_choice=question.choice_set.get(pk=request)
+        selected_choice=question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html',{
-            'question': question;
+            'question': question,
             'error_message': "no seleccionaste ninguna opción"
         })
     else:
         selected_choice.votes += 1
         selected_choice.save()
         return HttpresponseRedirect(reverse(
-            'polls:result', args=(question.id,)))I
+            'polls:result', args=(question.id,)))
